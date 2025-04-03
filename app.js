e64277c8156c945b04752e262b95a1bde364ee9d
DeepainSaini <deepainsaini111@gmail.com>
2025-04-04 02:26:27 +0530

    Designing the Initial Database Schema for a Bus Booking System(Task)

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app  = express();
 const connection = mysql.createConnection({
     host : 'localhost',
     user : 'root',
-    password : '964999',
+    password : '',
     database : 'testdb'
 })
 
@@ -18,12 +18,34 @@ connection.connect((err)=>{
 
     console.log('connection has been created');
 
-    const creationQuery = `create table students(
+    const creationQuery = `create table Users (
         
          id INT AUTO_INCREMENT PRIMARY KEY,
          name VARCHAR(20),
-         emil VARCHAR(20)
+         email VARCHAR(20)
     )`
+
+    const creationQuery1 = `create table Buses (
+        
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        busNumber VARCHAR(20),
+        totalSeats INT,
+        availableSeats INT
+    )`
+
+    const creationQuery2 = `create table Bookings (
+        
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        seatNumber INT
+    )`
+
+    const creationQuery3 = `create table Payments (
+        
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        amountPaid VARCHAR(20),
+        paymentStatus VARCHAR(20)
+    )`
+
 
     connection.execute(creationQuery,(err)=>{
 
@@ -33,7 +55,40 @@ connection.connect((err)=>{
             return;
         }
 
-        console.log('table has been created');
+        console.log(' Users table has been created');
+    });
+
+    connection.execute(creationQuery1,(err)=>{
+
+        if(err){
+            console.log(err);
+            connection.end();
+            return;
+        }
+
+        console.log('Buses table has been created');
+    });
+
+    connection.execute(creationQuery2,(err)=>{
+
+        if(err){
+            console.log(err);
+            connection.end();
+            return;
+        }
+
+        console.log('Bookings table has been created');
+    });
+
+    connection.execute(creationQuery3,(err)=>{
+
+        if(err){
+            console.log(err);
+            connection.end();
+            return;
+        }
+
+        console.log('Payments table has been created');
     });
 })
 
